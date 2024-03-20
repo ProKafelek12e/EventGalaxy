9af3af5b725240807afea9386a4cda90111c9303
ProKafelek12e <pro.kafelek@gmail.com>
2024-03-20 17:22:09 +0100

    tab list

--- list.php
+++ list.php
@@ -1,9 +1,12 @@
-<ul>
-    <li><a href="./index.php">Home</a></li>
-    <li><a href="./calendar.php">Calendar</a></li>
-    <li><a href="./login.php">Login</a></li>
-    <li><a href="./register.php">Register</a></li>
-    <?php if($_SESSION['Permission']=='adm'||$_SESSION['Permission']=='wrk') echo '<li><a href="./events.php">Manage Events</a></li>' ?>
-    <?php if($_SESSION['Permission']=='adm') echo '<li><a href="./permission.php">Permissions</a></li>' ?>
-    <?php if($_SESSION['IsLoged']==true) echo '<li><a href="./logout.php">Log out</a></li>' ?>
+<!-- <ul> -->
+    <!-- <?php if($_SESSION['Permission']=='adm'||$_SESSION['Permission']=='wrk') echo '<li><a href="./events.php">Manage Events</a></li>' ?> -->
+    <!-- <?php if($_SESSION['Permission']=='adm') echo '<li><a href="./permission.php">Permissions</a></li>' ?> -->
+    <!-- <?php if($_SESSION['IsLoged']==true) echo '<li><a href="./logout.php">Log out</a></li>' ?> -->
+<!-- </ul> -->
+
+
+<ul id="tabs">
+    <li class="tab"><a href="./index.php"><img src="./icons/home.svg"><h3>Home</h3></a></li>
+    <li class="tab"><a href="./index.php"><img src="./icons/calendar.svg"><h3>Calendar</h3></a></li>
+    <li class="tab"><a href="./index.php"><img src="./icons/logIn.svg"><h3>Log in</h3></a></li>
 </ul>
