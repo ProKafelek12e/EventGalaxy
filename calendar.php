e22c696c64aefdfe2a28d9ddbc51c33ab519f4f3
ProKafelek12e <pro.kafelek@gmail.com>
2024-03-21 18:32:11 +0100

    calendar page

--- calendar.php
+++ calendar.php
@@ -20,5 +20,105 @@ session_start()
         <img src="./icons/logo.svg" >
         <?php include 'list.php' ?>
     </div>
+    <div id="calendar">
+
+        <?php
+            function formatTime($time){
+                return '<span class="timeContainer"><span>'.substr($time,0,2).'</span>'.'<span class="top">'.substr($time,3,2).'</span></span>';
+            }
+
+            // Get the current year and month
+            $currentYear = date("Y");
+            $currentMonth = date("n");
+            $events = [];
+            $event_count = 0;
+            $conn = mysqli_connect('localhost','root','','szps');
+            
+            if(!$conn){
+                die(mysqli_connect_error($conn));
+            }
+            else{
+                $sql = "SELECT * FROM events WHERE date LIKE '$currentYear%$currentMonth-__' ORDER BY date asc, time asc";
+                $result = mysqli_query($conn,$sql);
+                if(mysqli_num_rows($result)>0){
+                    while( $row = mysqli_fetch_assoc($result)){
+                        $events[] = ['id' => $row['event_id'], 'name' => $row['name'], 'description' => $row['description'], 'date' => $row['date'], 'time' => $row['time']];
+                    }
+                }
+            }
+
+            // Get the number of days in the current month
+            $numDaysInMonth = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+
+            // Get the first day of the month
+            $firstDayOfMonth = date("N", mktime(0, 0, 0, $currentMonth, 1, $currentYear));
+
+            // Output the calendar
+            echo "<table>";
+            echo "<tr>";
+
+            // Fill in blank cells for days before the first day of the month
+            for ($i = 1; $i < $firstDayOfMonth; $i++) {
+                echo "<td></td>";
+            }
+
+            // Fill in the days of the month
+            for ($day = 1; $day <= $numDaysInMonth; $day++) {
+                if($firstDayOfMonth==7){
+                    echo "<td><h4 class='sunday'>$day</h4>";
+                    if($event_count<count($events)){
+                        if(substr($events[$event_count]['date'],-2)==$day){
+                            echo '<div class="event">';
+                            echo '<span>';
+                            echo '<h5>'.$events[$event_count]['name'].'</h5>';
+                            echo '<h5>'.formatTime($events[$event_count]['time']).'</h5>';
+                            echo '</span>';
+                            echo '<hr color="#f2c70d">';
+                            echo '</div>';
+
+                            $event_count+=1;
+                        }
+                    }
+                    echo "</td>";
+                }
+                else{
+                    echo "<td><h4>$day</h4>";
+                    if($event_count<count($events)){
+                        if(substr($events[$event_count]['date'],-2)==$day){
+
+                            echo '<div class="event">';
+                            echo '<span>';
+                            echo '<h5>'.$events[$event_count]['name'].'</h5>';
+                            echo '<h5>'.formatTime($events[$event_count]['time']).'</h5>';
+                            echo '</span>';
+                            echo '<hr color="#f2c70d">';
+                            echo '</div>';
+                        
+                            $event_count+=1;
+
+                        }
+                    } 
+                    echo "</td>";
+                }
+                $firstDayOfMonth++;
+                if ($firstDayOfMonth > 7) {
+                    echo "</tr><tr>";
+                    $firstDayOfMonth = 1;
+                }
+            }
+
+            // Fill in blank cells for remaining days of the week
+            while ($firstDayOfMonth > 1 && $firstDayOfMonth <= 7) {
+                echo "<td></td>";
+                $firstDayOfMonth++;
+            }
+
+            echo "</tr>";
+            echo "</table>";
+        ?>
+
+
+    </div>
+
 </body>
 </html>
--- szps.sql
+++ szps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2024 at 12:19 PM
+-- Generation Time: Mar 21, 2024 at 06:31 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -20,6 +20,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `szps`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `name`, `description`, `date`, `time`) VALUES
+(1, 'TEST EVENT', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-28', '14:55:00'),
+(2, 'concert', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-19', '12:00:00'),
+(3, 'workshop', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-20', '10:00:00'),
+(4, 'conference', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-21', '09:30:00'),
+(6, 'charity fundraiser', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-22', '14:00:00'),
+(7, 'sports tournament', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-24', '13:30:00'),
+(8, 'movie screening', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-25', '18:00:00'),
+(9, 'cooking class', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-26', '16:30:00'),
+(10, 'dance competition', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n', '2024-03-27', '15:00:00');
 
 -- --------------------------------------------------------
 
@@ -49,6 +78,12 @@ INSERT INTO `users` (`user_id`, `login`, `password`, `permission`) VALUES
 --
 
 --
+-- Indeksy dla tabeli `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`);
+
+--
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
@@ -58,6 +93,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
