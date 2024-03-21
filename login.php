dc637c6d1dfdda30a9e44c70c0f3a31f70acc572
ProKafelek12e <pro.kafelek@gmail.com>
2024-03-21 12:19:38 +0100

    working login page

--- login.php
+++ login.php
@@ -22,23 +22,67 @@ session_start()
     <div  id="form">
         <form action="" method="post">
             <span>
-                <input type="text" class="input_style" placeholder="Login">
-                <input type="password" class="input_style" placeholder="Password"> 
+                <input type="text" name="login" class="input_style" placeholder="Login">
+                <input type="password" name="password" class="input_style" placeholder="Password"> 
             </span>
             <span>
                 <input type="submit" name = "Up" value="Sign Up" class="button b"> 
                 <input type="submit" name = "In" value="Sign In" class="button f">
             </span>
         </form>
-    </div>
-    <?php
+        <?php
+    //from filled?
+    if(isset($_POST['login']) && isset($_POST['password'])){
+        $login = $_POST['login'];
+        $password = $_POST['password'];
+        
         if(isset($_POST['Up'])){
-            // insert into -> alert "acc created"
+            
+            $conn = mysqli_connect('localhost','root','','szps');
+            
+            if(!$conn){
+                die(mysqli_connect_error($conn));
+            }            
+            else{
+                $check_sql = "SELECT * FROM users WHERE login='$login'";
+                $check_result = mysqli_query($conn,$check_sql);
+                if(mysqli_num_rows($check_result)>0){
+                    echo "<h4>Login already taken</h4>";
+                }
+                else{
+                    $sql = "INSERT INTO `users`(`login`, `password`, `permission`) VALUES ('$login','".md5($password)."','usr')";
+                    $result = mysqli_query($conn,$sql);
+                    echo "<h4>Succesfully signed up</h4>";
+                    mysqli_close($conn);
+                }
+            }
         }
+        
+        
+        
         elseif(isset($_POST['In'])){
-            header('Location: ./index.php');
-            //select if(rows>0) -> log in 
+            
+            $conn = mysqli_connect('localhost','root','','szps');
+            
+            if(!$conn){
+                die(mysqli_connect_error($conn));
+            }        
+            else{
+                $check_sql = "SELECT * FROM users WHERE login='$login' AND password = '".md5($password)."'";
+                $check_result = mysqli_query($conn,$check_sql);
+                if(mysqli_num_rows($check_result)>0){
+                    echo "<h4>Logged in</h4>";
+                    $row = mysqli_fetch_assoc($check_result);
+                    
+                    $_SESSION['Permission'] = $row['permission'];
+                    $_SESSION['IsLogged'] = 1;
+                    
+                    header('Location: ./index.php');
+                }
+            }
         }
+    }
     ?>
+    </div>
 </body>
 </html>
---
+++ szps.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 21, 2024 at 12:19 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `szps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `login` text NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `permission` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `login`, `password`, `permission`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'adm'),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'usr'),
+(3, 'a', '92eb5ffee6ae2fec3ad71c777531578f', 'usr'),
+(4, 'pog', '5bb50314c7d970ce6cb07afb583c4c9d', 'usr');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `login` (`login`) USING HASH;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
