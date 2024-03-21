86f15c8acd7b86333ef01eb0e872da3b5f3f1e4d
ProKafelek12e <pro.kafelek@gmail.com>
2024-03-21 13:48:06 +0100

    favicon + delete unsed file

--- index.php
+++ index.php
@@ -6,7 +6,8 @@ session_start()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Event Manager</title>
+    <title>Event Galaxy</title>
+    <link rel="icon" href="./icons/logo.svg">
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
